7d5f2048526ea2497175d1dc9ab156b5fe144868
Yuya Hirano <v322sk@gmail.com>
2023-01-11 07:50:28 -0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  // const urlAPI = "https://xzvrefmkp4.execute-api.ap-northeast-1.amazonaws.com/dev/twitter?query=chainsawman";
+
+  const [searchWord, setSearchWord] = useState("");
+
+  const createUrl = (searchWord: string) => {
+    const api = "https://xzvrefmkp4.execute-api.ap-northeast-1.amazonaws.com/dev/twitter?query=";
+    let urlAPI = api.concat(searchWord);
+    console.log(urlAPI);
+    return urlAPI;
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    //名前が保存されたか確認するlog
+    console.log(searchWord);
+    clickHandler(searchWord);
+  };
+
+  const handleChangeWord = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchWord(e.target.value);
+  };
+
+  const clickHandler = async (searchWord: string) => {
+    try {
+      let awsAPI = createUrl(searchWord);
+      const res = await axios.get(awsAPI);
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+    console.log("hello");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world!</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          keyword:
+          {/* inputタグに、入力された値を保存する関数を書く */}
+          <input type="text" name="keyword" value={searchWord} onChange={handleChangeWord} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
